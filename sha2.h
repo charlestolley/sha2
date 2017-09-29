b802d9cc464dc2a0abcfcfaffa2d4ec14ae8eeb8
Charles Tolley <charlestolley@gmail.com>
2017-09-29 13:44:37 -0600

    Added #define for hash output lengths

--- sha2.h
+++ sha2.h
@@ -4,22 +4,25 @@
 #include <stdlib.h>
 #include "rotate.h"
 
+#define SHA224_LENGTH 28
+#define SHA256_LENGTH 32
+
 typedef enum {
 	SHA224, /*requires 28 bytes*/
-	SHA256 /*requires 32 bytes*/
+	SHA256  /*requires 32 bytes*/
 } hash_alg;
 
 extern const unsigned int k[];
 
-/*takes a char array of size 4 big endian and stores it to an int*/
+/* takes a char array of size 4 big endian and stores it to an int */
 static inline unsigned int ctoi(const unsigned char * const);
 
-/* turns integer to big-endian char[4]*/
+/* turns integer to big-endian char[4] */
 static inline void itoc(unsigned int, char * const);
 
-/* stores a long to big-endian char[8]*/
+/* stores a long to big-endian char[8] */
 static inline void ltoc(unsigned long, char * const);
 
-/*hash must be an array of at least the size specified for h (see hash_alg typedef)*/
+/* hash must be an array of at least the size specified for the given algorithm */
 void sha2(const unsigned char * const, const unsigned long, unsigned char * const, hash_alg);
 #endif
